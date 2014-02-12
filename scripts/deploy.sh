1ea2235b656701c474d289de81e650b1bdd39494
Florian Waibel <fwaibel@eclipsesource.com>
2014-02-13 00:03:23 +0100

    Fixes path handling in the deployment helper deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-export LOCAL_CHARM_REPO=${LOCAL_CHARM_REPO-~/charms/precise}
+export LOCAL_CHARM_REPO=${LOCAL_CHARM_REPO-${HOME}/charms}
 
 export CHARM_NAME=instauth
 export CHARM_HOME=instauth-charm
 
-echo "Deploying charm ${CHARM_NAME} from ${CHARM_HOME} to ${LOCAL_CHARM_REPO}:"
+echo "Deploying charm ${CHARM_NAME} from ${LOCAL_CHARM_REPO}:"
 
 juju deploy --repository=${LOCAL_CHARM_REPO} local:precise/${CHARM_NAME}
